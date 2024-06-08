889f6fa1d3e35b5d42ec071287f31cd9d34cbd33
GWANG <GWANG@GWANG_HOME>
2024-06-09 00:04:42 +0900

    2024/06/09 KIM 프로젝트시작

---
+++ FitTracker/src/main/java/com/site/FitTracker/FitTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.site.FitTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitTrackerApplication.class, args);
+	}
+
+}
---
+++ FitTracker/src/test/java/com/site/FitTracker/FitTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.site.FitTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
